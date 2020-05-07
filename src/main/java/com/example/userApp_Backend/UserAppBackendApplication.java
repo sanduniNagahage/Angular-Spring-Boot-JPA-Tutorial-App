2af8d8c53f1c06299d91559c2fd8173cb4ff7c49
Sanduni Nagahage <sanduninagahage@gmail.com>
2020-05-07 13:08:55 +0530

    back end initialized- spring boot and JPA

---
+++ src/main/java/com/example/userApp_Backend/UserAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.userApp_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAppBackendApplication.class, args);
+	}
+
+}

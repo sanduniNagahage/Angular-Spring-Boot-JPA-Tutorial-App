91dffaa2c7dec1173642155c0e70b08663357585
Sanduni Nagahage <sanduninagahage@gmail.com>
2020-05-07 16:27:40 +0530

    completed the controller class

---
+++ src/main/java/com/example/userApp_Backend/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.userApp_Backend.controller;
+
+import java.util.List;
+
+import com.example.userApp_Backend.model.User;
+import com.example.userApp_Backend.repository.UserRepository;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        return (List<User>)userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    void createUser(@RequestBody User user){
+        userRepository.save(user);
+    }
+}
